8beb46ed9d1b15712576f5be6a8f2ce1e3c1395c
TechOgede <sammonius@live.com>
2023-06-21 08:09:28 -0700

    prepares a MySQL server

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+-- This scrip prepares a MySQL server
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwD';
+
+USE hbnb_dev_db;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+GRANT  SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
