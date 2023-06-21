dbe889bc5485fba83d4c33e61e8e6d33716913fe
TechOgede <sammonius@live.com>
2023-06-21 15:07:04 -0700

    DBStorage - States and Cities

---
+++ models/engine/db_storage.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+''' This module defines the Database storage engine '''
+
+
+import os
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+
+class DBStorage:
+    ''' This class defines the Database storage object '''
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        ''' Initialises instance attrs '''
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:3306/{}'.
+                                      format(os.getenv('HBNB_MYSQL_USER'),
+                                             os.getenv('HBNB_MYSQL_PWD'),
+                                             os.getenv('HBNB_MYSQL_HOST'),
+                                             os.getenv('HBNB_MYSQL_DB')),
+                                      pool_pre_ping=True)
+
+        if os.getenv('HBNB_ENV') == 'test':
+            metadata = MetaData(bind=self.__engine)
+            metadata.drop_all()
+
+    def all(self, cls=None):
+        ''' Return all types of objects or only objects of type "cls"
+            from the database
+        '''
+        _objs = {}
+
+        if cls:
+            objs_list = self.__session.query(cls).all()
+            for obj in objs_list:
+                key = f'{obj.__class__.__name__}.{obj.id}'
+                _objs[key] = obj
+
+        else:
+            metadata = MetaData(bind=self.__engine)
+            metadata.reflect()
+            tables = metadata.tables.keys()
+            for table in tables:
+                objs_list = self.__session.query(metadata.tables[table]).all()
+                print(objs_list)
+                for obj in objs_list:
+                    key = f'{obj.__class__.__name__}.{obj.id}'
+                    _objs[key] = obj
+
+        return _objs
+
+    def new(self, obj):
+        ''' Adds the object to the current database session '''
+        self.__session.add(obj)
+
+    def save(self):
+        ''' Commits all changes of the current database session '''
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        ''' Deletes from the current database session'''
+        if obj:
+            self.__session.delete(obj, synchronize_session=False)
+
+    def reload(self):
+        '''Creates all tables in the database and creates the current
+           database session
+        '''
+        from models.base_model import Base, BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+        Base.metadata.create_all(self.__engine)
+        sesh_fact = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sesh_fact)
+        self.__session = Session()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import *
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    from models import storage
+    name = Column(String(128), nullable=False)
+
+    if storage.__class__.__name__ == 'DBStorage':
+        cities = relationship('City', backref='state')
+    else:
+        @property
+        def cities(self):
+            '''Getter fpr the cities attr '''
+            cities_list = []
+            cities = storage.all(City)
+            for key, value in cities.items():
+                if value.state_id == self.id:
+                    cities_list.append(value)
+
+            return cities_list
