f8b7a777e650cd4a7788c64bf1280808fe8c5026
TechOgede <sammonius@live.com>
2023-07-22 06:02:31 -0700

    returns page if int is in route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+'''Starts a Flask web application'''
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    ''' Returns Hello HBNB'''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    ''' Returns HBNB '''
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C(text):
+    ''' Returns custom text '''
+    text = text.replace('_', ' ')
+    return f'C {text}'
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def Python(text='is cool'):
+    ''' Returns custom text '''
+    text = text.replace('_', ' ')
+    return f'Python {text}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    ''' Returns custom text containing integer '''
+    return f'{n} is a number'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
