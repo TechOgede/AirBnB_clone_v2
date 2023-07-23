62df3d1a851996b510a8469a624c0c243eef9d9c
TechOgede <sammonius@live.com>
2023-07-23 07:15:10 -0700

    fix

--- models/place.py
+++ models/place.py
@@ -10,13 +10,13 @@ class Place(BaseModel, Base):
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), default='N/A')
+    description = Column(String(1024))
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, default='N/A')
-    longitude = Column(Float, default='N/A')
+    latitude = Column(Float)
+    longitude = Column(Float)
     reviews = relationship('Review', cascade='all', backref='place')
 
     @property
